f6c4f38e88503515754d91f3df53b41c4832f6d8
Gabriel Medina <gmedina197@gmail.com>
2021-08-19 18:13:59 -0300

    adding citation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cli_arguments import get_args
 args = get_args()
 
 if args.version:
-    print("Version 0.0.2alpha")
+    print("Version 1.0.0")
 elif args.destination:
     send_echo(args.destination, verbose=args.verbose)
 else:
